f3829de81a047de5b33d355f081f5a300b82d368
junjae92 <jaejuni902@gmail.com>
2026-04-08 02:46:13 +0900

    Update account.js

--- routes/account.js
+++ routes/account.js
@@ -373,7 +373,7 @@ router.post('/balance/sync', async (req, res) => {
     const fillsResponse = await okxApi.getFills('', '', 200);
     
     let reconstructedHistory = [];
-    const initialDeposit = 103.18;
+    const initialDeposit = 423.80;
     let runningBalance = initialDeposit;
     
     if (billsResponse.data && billsResponse.data.length > 0) {
